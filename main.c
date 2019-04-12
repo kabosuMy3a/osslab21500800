76a53f4dfba771834c68118c325b64469506b530
kabosuMy3a <my3a3380@gmail.com>
2019-04-12 10:52:49 +0900

    OSS LAB 10분컷

---
+++ main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void sub1() ;
+void sub2() ;
+void sub3() ;
+void sub4() ;
+
+
+void main(){
+
+	
+	sub1();
+	sub2();
+	sub3();
+	sub4();
+
+
+
+}
+
+
+
+sub2() {
+
+
+
+	printf("집가고싶다\n") ;
+
+
+
+
+
+}
